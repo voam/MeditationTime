eb5a1f946aa61dd7299d1b5903b37b0d69f70f80
sujun <chars193@gmail.com>
2016-11-29 11:22:31 +0000

    video implemented

--- app/src/main/java/com/meditation/metime/Music.java
+++ app/src/main/java/com/meditation/metime/Music.java
@@ -1,13 +1,18 @@
 package com.meditation.metime;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Music extends AppCompatActivity {
 
+    MediaPlayer music1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        music1= MediaPlayer.create(this, R.raw.dark_knight_audio);
     }
 }
