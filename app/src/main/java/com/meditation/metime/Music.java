a92a8713bd86875e3bc8a714ac7071e8a5d5516f
Tony O'Rourke <sonicwrx@gmail.com>
2019-06-07 19:41:59 -0400

    refactor music activities into one activity

--- app/src/main/java/com/meditation/metime/Music.java
+++ app/src/main/java/com/meditation/metime/Music.java
@@ -10,6 +10,7 @@
 package com.meditation.metime;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,114 +29,124 @@ public class Music extends BaseActivityWithDrawer {
 
         // sets the progress layout to the BaseActivityWithDrawer
         LayoutInflater.from(this).inflate(R.layout.activity_music, getFrame());
-
+        Resources res = getResources();
+        String[] song_titles = res.getStringArray(R.array.songs_array);
         // title 1
         RelativeLayoutButton m1_btn = new RelativeLayoutButton(this,R.id.M1_btn);
-        m1_btn.setText(R.id.button_text, getResources().getText(R.string.title_1));
+      //  m1_btn.setText(R.id.button_text, getResources().getText(R.string.title_1));
+        m1_btn.setText(R.id.button_text, song_titles[0]);
         m1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_darbari.class);
-                startActivity(intent);
+                nextActivity(0);
+
             }
         });
 
         // title 2
         RelativeLayoutButton m2_btn = new RelativeLayoutButton(this,R.id.M2_btn);
-        m2_btn.setText(R.id.button_text, getResources().getText(R.string.title_2));
+       // m2_btn.setText(R.id.button_text, getResources().getText(R.string.title_2));
+        m2_btn.setText(R.id.button_text, song_titles[1]);
         m2_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_jaijaivanti.class);
-                startActivity(intent);
+                nextActivity(1);
             }
         });
 
         // title 3
         RelativeLayoutButton m3_btn = new RelativeLayoutButton(this,R.id.M3_btn);
-        m3_btn.setText(R.id.button_text, getResources().getText(R.string.title_3));
+       // m3_btn.setText(R.id.button_text, getResources().getText(R.string.title_3));
+        m3_btn.setText(R.id.button_text, song_titles[2]);
         m3_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_freeforever.class);
-                startActivity(intent);
+                nextActivity(2);
             }
         });
 
         // title 4
         RelativeLayoutButton m4_btn = new RelativeLayoutButton(this,R.id.M4_btn);
-        m4_btn.setText(R.id.button_text, getResources().getText(R.string.title_4));
+      //  m4_btn.setText(R.id.button_text, getResources().getText(R.string.title_4));
+        m4_btn.setText(R.id.button_text, song_titles[3]);
         m4_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_hiddenwings.class);
-                startActivity(intent);
+                nextActivity(3);
             }
         });
 
         // title 5
         RelativeLayoutButton m5_btn = new RelativeLayoutButton(this,R.id.M5_btn);
-        m5_btn.setText(R.id.button_text, getResources().getText(R.string.title_5));
+       // m5_btn.setText(R.id.button_text, getResources().getText(R.string.title_5));
+        m5_btn.setText(R.id.button_text, song_titles[4]);
         m5_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_highernature1.class);
-                startActivity(intent);
+                nextActivity(4);
             }
         });
 
         // title 6
         RelativeLayoutButton m6_btn = new RelativeLayoutButton(this,R.id.M6_btn);
-        m6_btn.setText(R.id.button_text, getResources().getText(R.string.title_6));
+       // m6_btn.setText(R.id.button_text, getResources().getText(R.string.title_6));
+        m6_btn.setText(R.id.button_text, song_titles[5]);
         m6_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_highernature2.class);
-                startActivity(intent);
+                nextActivity(5);
             }
         });
 
         // title 7
         RelativeLayoutButton m7_btn = new RelativeLayoutButton(this,R.id.M7_btn);
-        m7_btn.setText(R.id.button_text, getResources().getText(R.string.title_7));
+        //m7_btn.setText(R.id.button_text, getResources().getText(R.string.title_7));
+        m7_btn.setText(R.id.button_text, song_titles[6]);
         m7_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_innocencebeyond.class);
-                startActivity(intent);
+                nextActivity(6);
             }
         });
 
         // title 8
         RelativeLayoutButton m8_btn = new RelativeLayoutButton(this,R.id.M8_btn);
-        m8_btn.setText(R.id.button_text, getResources().getText(R.string.title_8));
+       // m8_btn.setText(R.id.button_text, getResources().getText(R.string.title_8));
+        m8_btn.setText(R.id.button_text, song_titles[7]);
         m8_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_origintruth_1.class);
-                startActivity(intent);
+                nextActivity(7);
             }
         });
 
         // title 9
         RelativeLayoutButton m9_btn = new RelativeLayoutButton(this,R.id.M9_btn);
-        m9_btn.setText(R.id.button_text, getResources().getText(R.string.title_9));
+        //m9_btn.setText(R.id.button_text, getResources().getText(R.string.title_9));
+        m9_btn.setText(R.id.button_text,  song_titles[8]);
         m9_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Music_origintruth_2.class);
-                startActivity(intent);
+                nextActivity(8);
             }
         });
+    }
+
+    private void nextActivity(int songIndex) {
+
+        Intent intent = new Intent(getApplicationContext(), Music_detail.class);
+        intent.putExtra(Music_detail.INTENT_SONG_INDEX, songIndex);
+        startActivity(intent);
+
     }
 
 }
---
+++ app/src/main/java/com/meditation/metime/Music_detail.java
@@ -0,0 +1,175 @@
+/**
+ *  MeDitationTime
+ *
+ *  Music_darbari.class: Controller class for a media player of the music section
+ *
+ * com.john.waveview.WaveView: by john990 from https://github.com/john990/WaveView
+ *
+ *  @version    1.0
+ *  @author     Meditate to Regenerate (meditatetoregenerate.org)
+ */
+
+package com.meditation.metime;
+
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.vending.expansion.zipfile.APKExpansionSupport;
+import com.android.vending.expansion.zipfile.ZipResourceFile;
+import com.john.waveview.WaveView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class Music_detail extends AppCompatActivity {
+
+    public static String INTENT_SONG_INDEX = "songIndex";
+
+    private String TAG = getClass().getSimpleName();
+    private SeekBar seekBar;
+    private WaveView waveView;
+    private boolean isPaused = false;
+    private long remaining=454000;
+    private MediaPlayer Mp;
+    private int songIndex = 0;
+
+    private static int[] songDurations = {
+            454, 560, 655,
+            260, 641,880,
+            473, 667, 603
+
+    };
+    private static String[] songs = {
+            "darbari.mp3", "jaijaivanti.mp3", "freeforever.mp3",
+            "hiddenwings.mp3", "highernature1.mp3", "highernature2.mp3",
+            "innocenceandbeyond.mp3", "originaltruth1.mp3", "originaltruth2.mp3"
+
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_detail);
+        Intent callingIntent = getIntent();
+        songIndex = callingIntent.getIntExtra(INTENT_SONG_INDEX,0);
+        remaining = songDurations[songIndex] * 1000;
+
+        final ToggleButton play_btn = (ToggleButton) findViewById(R.id.p_p);
+       // Mp= MediaPlayer.create(this, R.raw.darbari);
+        waveView = (WaveView) findViewById(R.id.wave_view);
+
+        String pathToFileInsideZip = songs[songIndex];
+        // Get a ZipResourceFile representing a specific expansion file
+
+//        String fileName = String.format("main.%d.%s.obb", 1, getPackageName());
+//        //         main.314159.com.example.app.obb
+//        String filePathToMyZip = this.getObbDir().toString() + fileName;
+
+        ZipResourceFile expansionFile = null;
+        InputStream fileStream = null;
+        Mp = new MediaPlayer();
+        // Get a ZipResourceFile representing a merger of both the main and patch files
+        try {
+            expansionFile =
+                    APKExpansionSupport.getAPKExpansionZipFile(
+                            getApplicationContext(),
+                            1, 0);
+            // Get an input stream for a known file inside the expansion file ZIPs
+            //   fileStream = expansionFile.getInputStream(pathToFileInsideZip);
+
+            //      ZipResourceFile.ZipEntryRO [] entries = expansionFile.getAllEntries();
+
+            //this should never happen as expansion file is downloaded either a) when app is installed or b) when app starts
+            //but it is useful in development stage when .obb expansion file may not be present or downloadable from Play store
+            if (expansionFile == null) {
+
+                Toast.makeText(this, R.string.play_file_error, Toast.LENGTH_LONG).show();
+                //go back to wheel menu
+                Intent intent = new Intent(getApplicationContext(), Music.class);
+                intent.putExtra("firstCall", true);
+                startActivity(intent);
+                finish();
+                return;
+            }
+
+            AssetFileDescriptor afd = expansionFile.getAssetFileDescriptor(pathToFileInsideZip);
+            Mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            Mp.prepare();
+
+        }
+        catch(IOException ex) {
+
+            Log.e(TAG, "Problem reading expansion file");
+            ex.printStackTrace();
+
+        }
+
+
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if(play_btn.isChecked()){
+                    isPaused=false;
+                }else{
+                    isPaused=true;
+                }
+
+                //the length of music
+                long mills = remaining;
+                //control of media
+                if(!isPaused){
+                    Mp.start();
+                }else{
+                    Mp.pause();
+                }
+
+                new CountDownTimer(remaining, 1000) { // adjust the milli seconds here
+
+                    public void onTick(long millisUntilFinished) {
+
+                        if(isPaused){
+                            cancel();
+                        }
+                        //set the level of waveview
+                        int progress = (int)((songDurations[songIndex] - (millisUntilFinished / 1000))*(100/ (float) songDurations[songIndex] ));
+
+                       // waveView.setProgress((int)((454-(millisUntilFinished / 1000))*(100/454.0)));
+
+                        waveView.setProgress(progress);
+                        remaining = millisUntilFinished;
+                        //finish activity if timer approach 2s
+                        if(remaining<2000){
+                            finish();
+                        }
+                    }
+                    public void onFinish() {
+                    }
+                }.start();
+
+
+            }
+        });
+
+
+
+    }
+
+    //stop the media id back button is pressed
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        Mp.stop();
+    }
+}
